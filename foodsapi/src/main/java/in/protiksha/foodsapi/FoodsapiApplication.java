260d93cbfffcfd811dd211a2c60cfa20752295bd
Protiksha Khanikar <pratikshakhanikar@gmail.com>
2026-07-21 00:35:42 +0530

    added Spring Boot backend setup

---
+++ foodsapi/src/main/java/in/protiksha/foodsapi/FoodsapiApplication.java
@@ -0,0 +1,13 @@
+package in.protiksha.foodsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsapiApplication.class, args);
+	}
+
+}
